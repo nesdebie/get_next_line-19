b3665ae0825988139c9eb465a1bad24cdf1a2923
Nestor Debiesme <nedebies@student.s19.be>
2021-08-13 04:44:32 +0200

    normv3

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: nedebies <nedebies@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/02 02:12:45 by nedebies          #+#    #+#             */
-/*   Updated: 2021/08/10 18:19:26 by nedebies         ###   ########.fr       */
+/*   Updated: 2021/08/13 04:38:42 by nedebies         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,6 @@
 # define GET_NEXT_LINE_H
 # include <stdlib.h>
 # include <unistd.h>
-# include <string.h>
-#ifndef BUFFER_SIZE
-#  define BUFFER_SIZE 32
-#endif
 
 size_t	ft_strlen(char *s);
 char	*ft_strdup(char *s1);
