ec25ee98ae4757d8292767d84cdf09fb0214925f
Nestor Debiesme <nesdebie@fu-r5-p5.s19.be>
2023-04-24 14:04:25 +0200

    remplacement de l'include _bonus.h par .h dans get_next_line.c

--- get_next_line.c
+++ get_next_line.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "get_next_line_bonus.h"
+#include "get_next_line.h"
 
 char	*ft_free(char *s1, char *s2)
 {
